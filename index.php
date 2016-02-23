75c28a2af1f42a6164cf95d5461686025d55c5d9
kinnesotan <kinmatw@dunwoody.edu>
2016-02-22 23:05:10 -0600

    Added main layout
    Layout comes with mock buttons, colors, ads, and content space.

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Best Offer Books</title>
+    <link rel="stylesheet" type="text/css" href="css/desktop.css" />
+</head>
+
+<body>
+    <!-- DESKTOP WRAPPER BEGIN -->
+    <div id="wrapper">
+        <div id="header-images">
+            <div id="header-left">
+                <a href="index.php"><img src="images/bestofferbookslogo.png" width="225" height="113" alt="Best Offer Books logo"></a>
+            </div>
+            <div id="header-right">
+                <img src="images/ad.png" width="728" height="90" alt="ad">
+            </div>
+        </div>
+        <div id="nav">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Buy</a></li>
+                <li><a href="">Sell</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+                <li><div id="space"></div></li>
+                <li><form action="#"><input class="submit" type="submit" value="Log In"></form></li>
+                <li><input class="search-box" type="text" name="search" placeholder="Search..."></li>
+            </ul>
+        </div>
+        <!-- MAIN CONTENT DIV HOLDS NEWS FEED AND RECENT LISTINGS -->
+        <div id="main-content">
+            <div id="left-content">
+                <div id="recent">
+                    
+                </div>
+                <div id="newsfeed">
+                    
+                </div>
+                <div id="featured">
+                    
+                </div>
+            </div>
+            <div id="right-ad">
+                    <h1>AD</h1>
+                    <br/>
+                    <h1>AD</h1>
+                    <br/>
+                    <h1>AD</h1>
+                    <br/>
+                    <h1>AD</h1>
+                    <br/>
+                    <h1>AD</h1>
+                    <br/>
+                    <h1>AD</h1>
+            </div>
+        </div>
+        
+    </div>
+    <!-- DESKTOP WRAPPER ENDS -->
+</body>
+</html>
