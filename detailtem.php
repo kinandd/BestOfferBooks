9d1b38c62b38f3ccb1495de56187b9b5e6154427
kluchrj <kluchrj@dunwoody.edu>
2016-02-25 14:02:50 -0600

    Add detail page

---
+++ detailtem.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Best Offer Books</title>
+    <link rel="stylesheet" type="text/css" href="css/desktop.css" />
+	<link rel="stylesheet" type="text/css" href="css/product.css" />
+</head>
+
+<body>
+    <!-- DESKTOP WRAPPER BEGIN -->
+    <div id="wrapper">
+        <div id="header-images">
+            <div id="header-left">
+                <a href="index.php"><img src="images/bestofferbookslogo.png" width="225" height="113" alt="Best Offer Books logo"></a>
+            </div>
+            <div id="header-right">
+                <img src="images/ad.png" width="728" height="90" alt="ad">
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Buy</a></li>
+                <li><a href="">Sell</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+                <li><div id="space"></div></li>
+                <li><form action="#"><input class="submit" type="submit" value="Log In"></form></li>
+                <li><input class="search-box" type="text" name="search" placeholder="Search..."></li>
+            </ul>
+        </nav>
+        <!-- MAIN CONTENT -->
+        <div id="main-content">
+            <div id="left-content">
+				
+                <div id="product">
+                    <div class="productImage">
+						<!-- http://blogs.spanishpod101.com/blog/2011/12/30/spanish-word-of-the-day-textbook-phrase/ -->
+						<img src="images/example_book.jpg" alt="Example Book" height="250" width="250" />
+					</div>
+					<div class="productInfo">
+						<h1 id="bookTitle">Book Name</h1>
+						<p>
+							<span id="label">Price:</span> $60<br />
+							<span id="label">ISBN:</span> 978-3-16-148410-0<br />
+							<span id="label">Condition:</span> Good<br />
+							<br />
+							<span id="label">Seller:</span> Joe Shmo<br />
+						</p>
+					</div>
+                </div>
+				<div id="related">
+                    <h1>Related</h1>
+					<p>
+						Fusce faucibus ante ut lectus aliquet, nec pharetra quam pulvinar. Aliquam vestibulum massa volutpat dui tincidunt, vitae congue arcu vehicula.
+					</p>
+                </div>
+				<div id="bookDescription">
+					<h1>Book Description</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec commodo sapien dolor, et viverra tellus mollis in. Duis ullamcorper ultrices eleifend. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi vitae pulvinar nisl, non congue nulla. In fermentum arcu id purus tempor, eu auctor nunc auctor. Praesent sit amet consectetur augue, in maximus erat. Nullam in lobortis leo. In mollis id lectus at commodo. Suspendisse placerat semper dolor, nec feugiat risus vehicula id. Cras dignissim diam ac nulla ornare euismod. Nam ac dolor porttitor, maximus elit placerat, aliquam dolor. 
+					</p>
+				</div>
+                <div id="sellerNotes">
+					<h1>Seller's Notes</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec commodo sapien dolor, et viverra tellus mollis in. Duis ullamcorper ultrices eleifend. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi vitae pulvinar nisl, non congue nulla. In fermentum arcu id purus tempor, eu auctor nunc auctor. Praesent sit amet consectetur augue, in maximus erat. Nullam in lobortis leo. In mollis id lectus at commodo. Suspendisse placerat semper dolor, nec feugiat risus vehicula id. Cras dignissim diam ac nulla ornare euismod. Nam ac dolor porttitor, maximus elit placerat, aliquam dolor. 
+					</p>
+				</div>
+            </div>
+            <div id="right-ad">
+                    <h1>AD</h1>
+                    <br/>
+                    <h1>AD</h1>
+                    <br/>
+                    <h1>AD</h1>
+                    <br/>
+                    <h1>AD</h1>
+                    <br/>
+                    <h1>AD</h1>
+                    <br/>
+                    <h1>AD</h1>
+            </div>
+        </div>
+        
+    </div>
+    <!-- DESKTOP WRAPPER ENDS -->
+</body>
+</html>
