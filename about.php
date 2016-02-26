2d80147886da4d3c2789f1831c0be96cb499c5c1
kinandd <kinandd@dunwoody.edu>
2016-02-26 11:14:11 -0600

    Added About Page and Page connections

---
+++ about.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Best Offer Books</title>
+    <link rel="stylesheet" type="text/css" href="css/about.css" />
+</head>
+
+<body>
+    <!-- DESKTOP WRAPPER BEGIN -->
+    <div id="wrapper">
+        <div id="header-images">
+            <div id="header-left">
+                <a href="index.php"><img src="images/bestofferbookslogo.png" width="225" height="113" alt="Best Offer Books logo"></a>
+            </div>
+            <div id="header-right">
+                <img src="images/ad.png" width="728" height="90" alt="ad">
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="">Buy</a></li>
+                <li><a href="">Sell</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="">Contact</a></li>
+                <li><div id="space"></div></li>
+                <li><form action="#"><input class="submit" type="submit" value="Log In"></form></li>
+                <li><input class="search-box" type="text" name="search" placeholder="Search..."></li>
+            </ul>
+        </nav>
+        <!-- MAIN CONTENT DIV HOLDS NEWS FEED AND RECENT LISTINGS -->
+        <div id="main-content">
+            <div id="left-content">
+				<div class="group">
+					<div id="matt">
+						<h1>MATT</h1>
+						<p>
+							Matt is a cool dude
+						</p>
+					</div>
+					<div id="mikey">
+						<h1>MIKEY</h1>
+						<p>
+							Mikey is also a cool dude
+						</p>
+					</div>
+					<div id="christian">
+						<h1>CHRISTIAN</h1>
+						<p>
+							christian is another pretty cool dude
+						</p>
+					</div>
+					<div id="andrew">
+						<h1>ANDREW</h1>
+						<p>
+							Andrew is alright I guess
+						</p>
+					</div>
+				</div>	
+            </div>
+            <div id="right-ad">
+                    <h1>AD</h1>
+                    <br/>
+                    <h1>AD</h1>
+                    <br/>
+                    <h1>AD</h1>
+                    <br/>
+                    <h1>AD</h1>
+                    <br/>
+                    <h1>AD</h1>
+                    <br/>
+                    <h1>AD</h1>
+            </div>
+        </div>
+        
+    </div>
+    <!-- DESKTOP WRAPPER ENDS -->
+</body>
+</html>
